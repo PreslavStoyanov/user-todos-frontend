cbb9737aadbdfbd1edbdb41df2af06141ae9f1e9
Преслав Стоянов <preslav0609@gmail.com>
2023-04-08 02:15:24 +0300

    Add routing

---
+++ src/components/AddToDo.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {Divider} from "antd";
+
+export default function AddToDo() {
+    return (
+        <div>
+            <Divider orientation="left">Add TODO</Divider>
+        </div>
+    );
+}
---
+++ src/components/AddUser.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {Divider} from "antd";
+
+export default function AddUser() {
+    return (
+        <div>
+            <Divider orientation="left">Add User</Divider>
+        </div>
+    );
+}
